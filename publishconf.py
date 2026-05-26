c7491e412d3e2e7c5bc45a629efa493db7ee7c4e
Thomas Perrot <thomas.perrot@tupi.fr>
2026-05-26 12:28:34 +0200

    conf: disable feeds in dev config and enable them in publish config
    Feeds were enabled in pelicanconf.py with SITEURL set to empty string,
    causing Pelican to warn about invalid feed URLs.
    
    Feed generation is now disabled (set to None) in the dev config and
    re-enabled with the proper SITEURL in publishconf.py.
    
    Signed-off-by: Thomas Perrot <thomas.perrot@tupi.fr>

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,16 @@ RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
 
+FEED_DOMAIN = SITEURL
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
+TRANSLATION_FEED_RSS = 'feeds/all-{lang}.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/{slug}.rss.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+TRANSLATION_FEED_ATOM = 'feeds/all-{lang}.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
 # Following items are often useful when publishing
 
 GOOGLE_ANALYTICS = "G-Q2GHH38HV9"
